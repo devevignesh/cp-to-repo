73189142a995b414c12974e6d1319e6e9c91bf36
devevignesh <vigneshe1992@gmail.com>
2021-06-02 22:19:42 +0530

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,34 +9,30 @@ DESTINATION_GITHUB_USERNAME="$2"
 DESTINATION_REPOSITORY_NAME="$3"
 USER_EMAIL="$4"
 USER_NAME="$5"
-DESTINATION_REPOSITORY_USERNAME="$6"
-TARGET_BRANCH="$7"
-COMMIT_MESSAGE="$8"
-API_TOKEN_GITHUB="$9"
+TARGET_BRANCH="$6"
+COMMIT_MESSAGE="$7"
+API_TOKEN_GITHUB="$8"
 
-# if [-z "$DESTINATION_REPOSITORY_USERNAME"]
+# if [-z "$DESTINATION_GITHUB_USERNAME"]
 # then 
-#     DESTINATION_REPOSITORY_USERNAME="$DESTINATION_REPOSITORY_USERNAME"
+#     DESTINATION_GITHUB_USERNAME="$DESTINATION_GITHUB_USERNAME"
 # fi
 
 # if [-z "$USER_NAME"]
 # then 
-#     USER_NAME="$DESTINATION_REPOSITORY_USERNAME"
+#     USER_NAME="$DESTINATION_GITHUB_USERNAME"
 # fi
 
-CLONE_DIR=$(mktemp -d)
+TEMP_DIR=$(mktemp -d)
 
 echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
-git clone -n "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --depth 1 "$CLONE_DIR"
+git clone -n "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_GITHUB_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --depth 1 "$TEMP_DIR"
+cd "$TEMP_DIR"
 git reset HEAD
 git checkout HEAD "src/index.html"
-
-TARGET_DIR=$(mktemp -d)
-
-mv "src/index.js" "$TARGET_DIR"
 
 # if [ ! -d "$SOURCE_DIRECTORY" ]
 # then 
@@ -44,14 +40,13 @@ mv "src/index.js" "$TARGET_DIR"
 # fi
 
 echo "Copy index.html to target git repo"
-cp "$SOURCE_DIRECTORY" "$TARGET_DIR"
+echo "$SOURCE_DIRECTORY"
+
+cp "$SOURCE_DIRECTORY" "$TEMP_DIR"/src/.
 cd "$TARGET_DIR"
 
 echo "Files that will be pushed"
 ls
-
-echo "git add"
-git add .
 
 echo "git commit"
 git commit -m "build file"
